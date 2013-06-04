4a2f503eaf22f19a58956c748f49c6e0bd319d2a
S515785 <S515785@NW26914.nwmissouri.edu>
2013-06-04 14:33:30 -0500

    Added a print statement (Abhyudaya Reddy Gurram)

--- src/gitassignment/GitAssignment.java
+++ src/gitassignment/GitAssignment.java
@@ -8,51 +8,50 @@ package gitassignment;
  *
  * @author suryachittineni
  */
-public class GitAssignment
-{
+public class GitAssignment {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args)
-	{
-		System.out.println("Surya Teja Chittineni");
-		System.out.println("Danam Mahender");
-		System.out.println("Rishi Chowdary Pentyala");
-		System.out.println("\nSatish Kumar Baswapuram");
-		System.out.println("Vidhatri Manoor");
-		System.out.println("Anusha Punugu");
-		System.out.println("Vineela Yedavalli");
-		System.out.println("sai siddarth yerrabacha");
-		System.out.println("Shruthi Reddy Gummadi");
-		System.out.println("Divya Sadhineni");
-		System.out.println("Sujith Kumar Tummala");
-		System.out.println("Abhinay Reddy Ragi");
-		System.out.println("Sravani Daparthi");
-		System.out.println("Chaitanya Sagar Peddaboodhi");
-                System.out.println("Poornima Bandari");
-                System.out.println("Shivanandana Aleti");
-                System.out.println("Garima Dhakal");
-            System.out.println("Harika Narendra");
-            System.out.println("Srinivas Kaleru");
-            System.out.println("Nishitha Reddy Nalla");
-            System.out.println("Nyruthi Pachika");
-		System.out.println("Abdul Khaliq Shaik");
-                System.out.println("Narayana Praveen");
-                System.out.println("Prthyusha Ragireddy");
-                System.out.println("Mohan Muralidhar Bikkina");
-                System.out.println("Sindhuri Mutyala");
-                System.out.println("Phani Kumar Kodavati");
-                System.out.println("Ayesha Shaik");
-                System.out.println("Santhi Kumari Popuri");
-                System.out.println("Krishna Muduganti");
-                System.out.println("Anusha Tumula");
-                System.out.println("Sradhika Chinthapatla");
-                System.out.println("Deepthi Nelavelli");
-                System.out.println("Soniya Mogre");
-                System.out.println("Pravallika Ponna");
-                System.out.println("Santhosh Palla");
-                System.out.println("Mrudula Yalamandala");
-                System.out.println("Rohith Krishna Sakamuri");
-	}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Surya Teja Chittineni");
+        System.out.println("Danam Mahender");
+        System.out.println("Rishi Chowdary Pentyala");
+        System.out.println("\nSatish Kumar Baswapuram");
+        System.out.println("Vidhatri Manoor");
+        System.out.println("Anusha Punugu");
+        System.out.println("Vineela Yedavalli");
+        System.out.println("sai siddarth yerrabacha");
+        System.out.println("Shruthi Reddy Gummadi");
+        System.out.println("Divya Sadhineni");
+        System.out.println("Sujith Kumar Tummala");
+        System.out.println("Abhinay Reddy Ragi");
+        System.out.println("Sravani Daparthi");
+        System.out.println("Chaitanya Sagar Peddaboodhi");
+        System.out.println("Poornima Bandari");
+        System.out.println("Shivanandana Aleti");
+        System.out.println("Garima Dhakal");
+        System.out.println("Harika Narendra");
+        System.out.println("Srinivas Kaleru");
+        System.out.println("Nishitha Reddy Nalla");
+        System.out.println("Nyruthi Pachika");
+        System.out.println("Abdul Khaliq Shaik");
+        System.out.println("Narayana Praveen");
+        System.out.println("Prthyusha Ragireddy");
+        System.out.println("Mohan Muralidhar Bikkina");
+        System.out.println("Sindhuri Mutyala");
+        System.out.println("Phani Kumar Kodavati");
+        System.out.println("Ayesha Shaik");
+        System.out.println("Santhi Kumari Popuri");
+        System.out.println("Krishna Muduganti");
+        System.out.println("Anusha Tumula");
+        System.out.println("Sradhika Chinthapatla");
+        System.out.println("Deepthi Nelavelli");
+        System.out.println("Soniya Mogre");
+        System.out.println("Pravallika Ponna");
+        System.out.println("Santhosh Palla");
+        System.out.println("Mrudula Yalamandala");
+        System.out.println("Rohith Krishna Sakamuri");
+        System.out.println("Abhyudaya Reddy Gurram");
+    }
 }
